6edaad1724432d890e58759e999cf0b8e3dbec6f
goeka <goeka1337@gmail.com>
2025-10-18 06:15:41 +0900

    fix(application): 리터럴 \\n 입력을 실제 개행으로 정규화하도록 하여 파싱 오류 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,11 +8,14 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         final String input = Console.readLine();
 
+        // 입력 정규화 (리터럴 "\n"을 실제 개행으로 정규화)
+        String normalizedInput = input.replace("\\n", "\n");
+
         try {
             //TODO: InputValidator에 사용자가 입력한 문자열 유효성 검증 위임
 
             // StringCalculator에 유효한 문자열 전달, 결과 반환받음
-            int result = StringCalculator.calculate(input);
+            int result = StringCalculator.calculate(normalizedInput);
 
             // 결과 출력
             System.out.println("결과 : " + result);

44628024f49cc280d9b8d9fed89cf667bd29a575
goeka <goeka1337@gmail.com>
2025-10-17 21:16:46 +0900

    feat(Application): 입력,출력 기능구현. 문자열 덧셈 계산기 메인흐름 완성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,28 @@
 package calculator;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        // 사용자 입력
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        final String input = Console.readLine();
+
+        try {
+            //TODO: InputValidator에 사용자가 입력한 문자열 유효성 검증 위임
+            //TODO: StringCalculator에 유효한 문자열 전달, 결과 반환받음
+
+            int result = 2; // 임시 값. 이후 StringCalculator의 결과로 교체할 것
+
+            // 결과 출력
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e){
+            // 사용자 입력오류를 처리하여 안내 메시지 출력 후 애플리케이션을 정상 종료
+            System.out.println("잘못된 입력입니다: " + e.getMessage());
+        } finally {
+            // 입력 리소스 정리
+            Console.close();
+        }
+
     }
 }

c9e12999eab1a25b933002e7b1f4201f238cbe38
goeka <goeka1337@gmail.com>
2025-10-19 19:32:34 +0900

    fix(application): IllegalArgumentException을 catch하지 않고 상위로 전파되도록 main 로직 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -11,22 +11,15 @@ public class Application {
         // 입력 정규화 (리터럴 "\n"과 실제 개행입력 모두 처리 가능하도록)
         String normalizedInput = input.replace("\\n", "\n");
 
-        try {
-            // 사용자가 입력한 문자열 유효성 검증
-            InputValidator.validateOrThrow(normalizedInput);
+        // 사용자가 입력한 문자열 유효성 검증
+        InputValidator.validateOrThrow(normalizedInput);
 
-            // 유효한 문자열 계산
-            int result = StringCalculator.calculate(normalizedInput);
+        // 유효한 문자열 계산
+        int result = StringCalculator.calculate(normalizedInput);
 
-            // 결과 출력
-            System.out.println("결과 : " + result);
-        } catch (IllegalArgumentException e) {
-            // 사용자 입력오류를 처리하여 안내 메시지 출력 후 애플리케이션을 정상 종료
-            System.out.println("잘못된 입력입니다: " + e.getMessage());
-        } finally {
-            // 입력 리소스 정리
-            Console.close();
-        }
+        // 결과 출력, 입력 리소스 정리
+        System.out.println("결과 : " + result);
+        Console.close();
 
     }
 }
